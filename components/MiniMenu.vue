4655a7a83fe15d2a841d9edaeb4e5d17e516fca9
migo1 <ertmigot@gmail.com>
2024-03-15 13:35:20 +0300

    chronic-care

---
+++ components/MiniMenu.vue
@@ -0,0 +1,132 @@
+<template>
+       <div class="col-lg-3 service-left-col sidebar">
+              <aside class="service-sidebar">
+                <aside class="widget post-list">
+                  <h2 class="widget-title">Our Service</h2>
+                  <div class="all-post-list">
+                    <ul>
+                      <li class="">
+                        <NuxtLink to="/services/chronic_care_management"
+                          >Chronic Care
+                        </NuxtLink>
+                      </li>
+                      <li>
+                        <NuxtLink to="/services/behavioral_in-home_care"
+                          >In-Home Care</NuxtLink
+                        >
+                      </li>
+                      <li>
+                        <NuxtLink to="/services/medication_management"
+                          >Medication Management</NuxtLink
+                        >
+                      </li>
+                      <li>
+                        <NuxtLink to="/services/remote_patient_monitoring"
+                          >Remote Patient Monitoring</NuxtLink
+                        >
+                      </li>
+                      <li>
+                        <NuxtLink to="/services/telemedicine_visits"
+                          >Telemedicine Visits</NuxtLink
+                        >
+                      </li>
+                    </ul>
+                  </div>
+                </aside>
+                <aside class="widget pbmit-service-ad">
+                  <div class="textwidget">
+                    <div class="pbmit-service-ads">
+                      <h5 class="pbmit-ads-subheding">Bookings</h5>
+                      <h4 class="pbmit-ads-subtitle">Ready for In-house healthcare ?</h4>
+                      <h3 class="pbmit-ads-title">Book with us!</h3>
+                      <div class="pbmit-ads-desc">
+                        <i class="pbmit-base-icon-phone-call-1"></i>+(123)
+                        1234-567-8901
+                      </div>
+                      <NuxtLink class="pbmit-btn" to="/book_appointment">
+                        <span class="pbmit-button-content-wrapper">
+                          <span
+                            class="pbmit-button-icon pbmit-align-icon-right"
+                          >
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              width="22.76"
+                              height="22.76"
+                              viewBox="0 0 22.76 22.76"
+                            >
+                              <title>black-arrow</title>
+                              <path
+                                d="M22.34,1A14.67,14.67,0,0,1,12,5.3,14.6,14.6,0,0,1,6.08,4.06,14.68,14.68,0,0,1,1.59,1"
+                                transform="translate(-0.29 -0.29)"
+                                fill="none"
+                                stroke="#000"
+                                stroke-width="2"
+                              ></path>
+                              <path
+                                d="M22.34,1a14.67,14.67,0,0,0,0,20.75"
+                                transform="translate(-0.29 -0.29)"
+                                fill="none"
+                                stroke="#000"
+                                stroke-width="2"
+                              ></path>
+                              <path
+                                d="M22.34,1,1,22.34"
+                                transform="translate(-0.29 -0.29)"
+                                fill="none"
+                                stroke="#000"
+                                stroke-width="2"
+                              ></path>
+                            </svg>
+                          </span>
+                          <span class="pbmit-button-text">Book now</span>
+                        </span>
+                      </NuxtLink>
+                    </div>
+                  </div>
+                </aside>
+                <!-- <aside class="widget">
+                  <h2 class="widget-title">Company profile</h2>
+                  <div class="textwidget">
+                    <div class="download">
+                      <div class="item-download">
+                        <a href="#" target="_blank" rel="noopener noreferrer">
+                          <span class="pbmit-download-content">
+                            <i
+                              class="pbmit-base-icon-pdf-file-format-symbol-1"
+                            ></i>
+                            Download Pdf File
+                          </span>
+                          <span class="pbmit-download-item">
+                            <i
+                              class="pbminfotech-base-icons pbmit-righticon pbmit-base-icon-download"
+                            ></i>
+                          </span>
+                        </a>
+                      </div>
+                      <div class="item-download">
+                        <a href="#" target="_blank" rel="noopener noreferrer">
+                          <span class="pbmit-download-content">
+                            <i class="pbmit-base-icon-doc-file-format"></i>
+                            Download Word File
+                          </span>
+                          <span class="pbmit-download-item">
+                            <i
+                              class="pbminfotech-base-icons pbmit-righticon pbmit-base-icon-download"
+                            ></i>
+                          </span>
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                </aside> -->
+              </aside>
+            </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>
