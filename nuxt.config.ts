bd49b38ab5c1e66168a6b5f5e8124fedeb422d8b
migo1 <ertmigot@gmail.com>
2024-04-02 11:28:19 +0300

    add-SEO-title

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,7 +6,7 @@ export default defineNuxtConfig({
 
   app: {
     head: {
-      title: "DC MEDICAL",
+      title: "dcmedicalsystem",
       link: [{ rel: "icon", type: "image/x-icon", href: "/images/dc_logo.png" }],
       script: [
         { src: "/js/jquery.min.js", tagPosition: "bodyClose" },
