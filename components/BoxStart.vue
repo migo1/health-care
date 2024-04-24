ad93cae69cf78eec47ca325893d1100591236b39
migo1 <ertmigot@gmail.com>
2024-04-25 01:22:27 +0300

    emergency-cases-widget

--- components/BoxStart.vue
+++ components/BoxStart.vue
@@ -1,113 +1,130 @@
 <template>
-    <section class="section-xl inner-box_area">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-6 col-lg-4 position-relative">
-                    <div class="pbmit-widget_icon">
-                        <div class="pbmit_icon">
-                            <i class="pbmit-xcare-icon pbmit-xcare-icon-appointment"></i>
-                        </div>
-                    </div>
-                    <div class="inner-box_style inner-box_1">
-                        <div class="pbmit-heading_title">
-                            <h5>Schedule Hours</h5>
-                        </div>
-                        <ul class="pbmit-timelist-list">
-                            <li>
-                                <span class="pbmit-timelist-li-title">Monday – Friday</span>
-                                <span class="pbmit-timelist-li-value">08:00 - 18:00</span>
-                            </li>
-                            <li>
-                                <span class="pbmit-timelist-li-title">Saturday</span>
-                                <span class="pbmit-timelist-li-value">09:30 - 17:30</span>
-                            </li>
-                            <li>
-                                <span class="pbmit-timelist-li-title">Sunday</span>
-                                <span class="pbmit-timelist-li-value">09:00 - 15:30</span>
-                            </li>
-                            <li>
-                                <span class="pbmit-timelist-li-title">24/7 Service Available</span>
-                                <span class="pbmit-timelist-li-value"></span>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-                <div class="col-md-6 col-lg-4 position-relative">
-                    <div class="pbmit-widget_icon">
-                        <div class="pbmit_icon">
-                            <i class="pbmit-xcare-icon pbmit-xcare-icon-placeholder"></i>
-                        </div>
-                    </div>
-                    <div class="inner-box_style inner-box_2">
-                        <div class="pbmit-heading_title">
-                            <h5>Our Location</h5>
-                        </div>
-                        <div class="pbmit-text_aditor">
-                            Search our locations to find the one nearest you Get the
-                            answers and assurance you deserve with accuracy you can trust.
-                        </div>
-                        <NuxtLink class="pbmit-btn pbmit-btn-outline" to="/contact_us">
-                            <span class="pbmit-button-content-wrapper">
-                                <span class="pbmit-button-icon pbmit-align-icon-right">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="22.76" height="22.76"
-                                        viewBox="0 0 22.76 22.76">
-                                        <title>black-arrow</title>
-                                        <path
-                                            d="M22.34,1A14.67,14.67,0,0,1,12,5.3,14.6,14.6,0,0,1,6.08,4.06,14.68,14.68,0,0,1,1.59,1"
-                                            transform="translate(-0.29 -0.29)" fill="none" stroke="#000"
-                                            stroke-width="2"></path>
-                                        <path d="M22.34,1a14.67,14.67,0,0,0,0,20.75" transform="translate(-0.29 -0.29)"
-                                            fill="none" stroke="#000" stroke-width="2"></path>
-                                        <path d="M22.34,1,1,22.34" transform="translate(-0.29 -0.29)" fill="none"
-                                            stroke="#000" stroke-width="2"></path>
-                                    </svg>
-                                </span>
-                                <span class="pbmit-button-text">Get Directions</span>
-                            </span>
-                        </NuxtLink>
-                    </div>
-                </div>
-                <div class="col-md-6 col-lg-4 position-relative">
-                    <div class="pbmit-widget_icon">
-                        <div class="pbmit_icon">
-                            <i class="pbmit-xcare-icon pbmit-xcare-icon-doctor"></i>
-                        </div>
-                    </div>
-                    <div class="inner-box_style inner-box_3">
-                        <div class="pbmit-bg_overlay"></div>
-                        <div class="pbmit-heading_title">
-                            <h5>Our Location</h5>
-                        </div>
-                        <div class="pbmit-text_aditor">
-                            Emergency Medical Services, more commonly known as EMS, is a
-                            system that responds
-                        </div>
-                        <div class="pbmit-ihbox-style-19">
-                            <div class="pbmit-ihbox-box">
-                                <div class="pbmit-ihbox-icon">
-                                    <div class="pbmit-ihbox-icon-wrapper">
-                                        <div class="pbmit-icon-wrapper pbmit-icon-type-icon">
-                                            <i class="pbmit-xcare-icon pbmit-xcare-icon-live-chat"></i>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="pbmit-ihbox-contents">
-                                    <h4 class="pbmit-element-heading">Emergency Cases</h4>
-                                    <h2 class="pbmit-element-title">1-800-123-4560</h2>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+  <section class="section-xl inner-box_area">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6 col-lg-4 position-relative">
+          <div class="pbmit-widget_icon">
+            <div class="pbmit_icon">
+              <i class="pbmit-xcare-icon pbmit-xcare-icon-appointment"></i>
             </div>
+          </div>
+          <div class="inner-box_style inner-box_1">
+            <div class="pbmit-heading_title">
+              <h5>Schedule Hours</h5>
+            </div>
+            <ul class="pbmit-timelist-list">
+              <li>
+                <span class="pbmit-timelist-li-title">Monday – Friday</span>
+                <span class="pbmit-timelist-li-value">08:00 - 18:00</span>
+              </li>
+              <li>
+                <span class="pbmit-timelist-li-title">Saturday</span>
+                <span class="pbmit-timelist-li-value">09:30 - 17:30</span>
+              </li>
+              <li>
+                <span class="pbmit-timelist-li-title">Sunday</span>
+                <span class="pbmit-timelist-li-value">09:00 - 15:30</span>
+              </li>
+              <li>
+                <span class="pbmit-timelist-li-title"
+                  >24/7 Service Available</span
+                >
+                <span class="pbmit-timelist-li-value"></span>
+              </li>
+            </ul>
+          </div>
         </div>
-    </section>
+        <div class="col-md-6 col-lg-4 position-relative">
+          <div class="pbmit-widget_icon">
+            <div class="pbmit_icon">
+              <i class="pbmit-xcare-icon pbmit-xcare-icon-placeholder"></i>
+            </div>
+          </div>
+          <div class="inner-box_style inner-box_2">
+            <div class="pbmit-heading_title">
+              <h5>Our Location</h5>
+            </div>
+            <div class="pbmit-text_aditor">
+              Search our locations to find the one nearest you Get the answers
+              and assurance you deserve with accuracy you can trust.
+            </div>
+            <NuxtLink class="pbmit-btn pbmit-btn-outline" to="/contact_us">
+              <span class="pbmit-button-content-wrapper">
+                <span class="pbmit-button-icon pbmit-align-icon-right">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="22.76"
+                    height="22.76"
+                    viewBox="0 0 22.76 22.76"
+                  >
+                    <title>black-arrow</title>
+                    <path
+                      d="M22.34,1A14.67,14.67,0,0,1,12,5.3,14.6,14.6,0,0,1,6.08,4.06,14.68,14.68,0,0,1,1.59,1"
+                      transform="translate(-0.29 -0.29)"
+                      fill="none"
+                      stroke="#000"
+                      stroke-width="2"
+                    ></path>
+                    <path
+                      d="M22.34,1a14.67,14.67,0,0,0,0,20.75"
+                      transform="translate(-0.29 -0.29)"
+                      fill="none"
+                      stroke="#000"
+                      stroke-width="2"
+                    ></path>
+                    <path
+                      d="M22.34,1,1,22.34"
+                      transform="translate(-0.29 -0.29)"
+                      fill="none"
+                      stroke="#000"
+                      stroke-width="2"
+                    ></path>
+                  </svg>
+                </span>
+                <span class="pbmit-button-text">Get Directions</span>
+              </span>
+            </NuxtLink>
+          </div>
+        </div>
+        <div class="col-md-6 col-lg-4 position-relative">
+          <div class="pbmit-widget_icon">
+            <div class="pbmit_icon">
+              <i class="pbmit-xcare-icon pbmit-xcare-icon-doctor"></i>
+            </div>
+          </div>
+          <div class="inner-box_style inner-box_3">
+            <div class="pbmit-bg_overlay"></div>
+            <div class="pbmit-heading_title">
+              <h5>Our Contacts</h5>
+            </div>
+            <div class="pbmit-text_aditor">
+              A house call involves a healthcare provider, visiting a patient at
+              their residence to offer medical assistance
+            </div>
+            <div class="pbmit-ihbox-style-19">
+              <div class="pbmit-ihbox-box">
+                <div class="pbmit-ihbox-icon">
+                  <div class="pbmit-ihbox-icon-wrapper">
+                    <div class="pbmit-icon-wrapper pbmit-icon-type-icon">
+                      <i
+                        class="pbmit-xcare-icon pbmit-xcare-icon-live-chat"
+                      ></i>
+                    </div>
+                  </div>
+                </div>
+                <div class="pbmit-ihbox-contents">
+                  <h4 class="pbmit-element-heading">Emergency Cases</h4>
+                  <h2 class="pbmit-element-title">1-202- 361-9660</h2>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
-<script lang="ts" setup>
-
-
-
-</script>
+<script lang="ts" setup></script>
 
 <style></style>
